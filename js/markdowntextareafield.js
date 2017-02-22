579f1eb88de9888cae6e9f1871c4b95d2f7b1294
Angus Mckay <mckayangus3@gmail.com>
2017-02-23 10:15:16 +1300

    RWD-2175 WIP

--- js/markdowntextareafield.js
+++ js/markdowntextareafield.js
@@ -1,15 +1,37 @@
 function initializeMarkdownEditor() {
-    jQuery.entwine('markdowntextareafield', function() {
-        jQuery('.bu_markdown').each(function() {
-            var simplemde = new SimpleMDE({
-                element: this,
-                spellChecker: false, // temporary
-                hideIcons: ["heading","image","side-by-side","preview","side-by-side","preview"],
-                promptURLs: true,
-                forceSync: true
+    if(typeof window.simplemdeInstances == 'undefined') {
+        window.simplemdeInstances = [];
+    }
+    var arrayLength = window.simplemdeInstances.length;
+    var flag = false;
+    if(arrayLength > 0) {
+        for(var i = 0; i < arrayLength; i++) {
+            jQuery('.bu_markdown').each(function(index, element) {
+                console.log(window.simplemdeInstances[i].element, jQuery(window.simplemdeInstances[i].element).attr('id'))
+
+                if(typeof window.simplemdeInstances[i].element !== 'undefined' && jQuery(window.simplemdeInstances[i].element).attr('id') ==  jQuery(element).attr('id')) {
+                    flag = true;
+                    return;
+                }
+            });
+        }
+    }
+    if (flag == false) {
+        jQuery.entwine('markdowntextareafield', function() {
+            jQuery('.bu_markdown').each(function(index, element) {
+                window.simplemdeInstances.push(
+                    new SimpleMDE({
+                        element: this,
+                        spellChecker: false, // temporary
+                        hideIcons: ["heading","image","side-by-side","preview","side-by-side","preview"],
+                        promptURLs: true,
+                        forceSync: true
+                    })
+                );
             });
         });
-    });
+    }
+
 }
 
 /**
@@ -24,7 +46,8 @@ jQuery(document).ready(function() {
  */
 jQuery(document).ajaxStop(function(event) {
     if (event.target['activeElement'].id == 'link-autocomplete') {
-       return false;
+        return false;
     }
+
     initializeMarkdownEditor();
 });
